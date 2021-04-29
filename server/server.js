6780a0b2cd1f9f8a01ea47611e8883eb5d44d066
SEONKYU\difke <somedaycode@gmail.com>
2021-04-30 00:18:09 +0900

    Fix. Header Login

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
-require('dotenv').config();
 
 const authRouter = require('./routes/auth.js');
+const callbackRouter = require('./routes/callback.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../build')));
-
 app.use('/auth', authRouter);
+app.use('/callback', callbackRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
-});
-
-app.get('/callback', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
