c1b91f61bd10d1d5c011be44bf3ce94e52df373f
SEONKYU\difke <somedaycode@gmail.com>
2021-04-30 00:25:31 +0900

    Fix. location of build index.html

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use('/auth', authRouter);
 app.use('/callback', callbackRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(port, () => {
